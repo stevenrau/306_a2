6e93bf60332d7bc6ab22f7aa37ba4662287d522e
Steven Rau <scr108@ismac7.usask.ca>
2015-10-06 14:15:39 -0600

    Created new project for a2
    -> Created new project for a2, using the assets from
       the recent tutorial as a starting point.

---
+++ a2_top_down_shooter/Assets/Scripts/Spawn_Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_Bullet : MonoBehaviour {
+
+	public GameObject bullet_prefab;
+
+	public GameObject spawned_bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+			print ("Fire");
+			spawned_bullet = Instantiate(bullet_prefab, transform.position, transform.rotation) as GameObject;
+			spawned_bullet.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(750, 0));
+		}
+	
+	}
+}
